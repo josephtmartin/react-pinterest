8b9bf8e28bfc03c775b6a46f68c7b9b58ff1eaf1
josephtmartin <goldglovejo13@gmail.com>
2020-11-17 14:42:35 -0600

    got update to work for boards

--- src/helpers/data/patchFBkeys.js
+++ src/helpers/data/patchFBkeys.js
@@ -1,23 +1,23 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const baseUrl = 'https://fir-pinterest.firebaseio.com/';
+// const baseUrl = 'https://fir-pinterest.firebaseio.com/';
 
-const patchBoards = () => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/boards.json`).then((response) => {
-    const keys = Object.keys(response.data);
-    keys.forEach((key) => {
-      axios.patch(`${baseUrl}/boards/${key}.json`, { firebaseKey: key });
-    });
-  }).catch((error) => reject(error));
-});
+// const patchBoards = () => new Promise((resolve, reject) => {
+//   axios.get(`${baseUrl}/boards.json`).then((response) => {
+//     const keys = Object.keys(response.data);
+//     keys.forEach((key) => {
+//       axios.patch(`${baseUrl}/boards/${key}.json`, { firebaseKey: key });
+//     });
+//   }).catch((error) => reject(error));
+// });
 
-const patchPins = () => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/pins.json`).then((response) => {
-    const keys = Object.keys(response.data);
-    keys.forEach((key) => {
-      axios.patch(`${baseUrl}/pins/${key}.json`, { firebaseKey: key });
-    });
-  }).catch((error) => reject(error));
-});
+// const patchPins = () => new Promise((resolve, reject) => {
+//   axios.get(`${baseUrl}/pins.json`).then((response) => {
+//     const keys = Object.keys(response.data);
+//     keys.forEach((key) => {
+//       axios.patch(`${baseUrl}/pins/${key}.json`, { firebaseKey: key });
+//     });
+//   }).catch((error) => reject(error));
+// });
 
-export { patchBoards, patchPins };
+// export { patchBoards, patchPins };
