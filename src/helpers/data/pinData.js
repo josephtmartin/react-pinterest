8c1fa3d7866835413d194127eaecba4cfb738a83
josephtmartin <goldglovejo13@gmail.com>
2020-11-23 18:54:44 -0600

    added demo folder with images

--- src/helpers/data/pinData.js
+++ src/helpers/data/pinData.js
@@ -22,14 +22,14 @@ const getPin = (pinId) => new Promise((resolve, reject) => {
 
 const getAllPins = (userId) => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/pins.json`).then((response) => {
-    const nonPrivateArray = Object.values(response.data).filter((resp) => resp.private === 'Public' || resp.userId === userId);
+    const nonPrivateArray = Object.values(response.data).filter((resp) => resp.private === 'Public' && resp.userId === userId);
     resolve(nonPrivateArray);
   }).catch((error) => reject(error));
 });
 
 const searchPins = (uid, term) => new Promise((resolve, reject) => {
   getAllPins().then((response) => {
-    const filteredArray = response.filter((resp) => resp.userId === uid || resp.private === 'Public');
+    const filteredArray = response.filter((resp) => resp.userId === uid && resp.private === 'Public');
     const searchResults = filteredArray.filter((resp) => resp.name.toLowerCase().includes(term) || resp.description.toLowerCase().includes(term));
     resolve(searchResults);
   }).catch((error) => reject(error));
